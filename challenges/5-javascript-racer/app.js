30b05ae3dce9fb61e1633ee39048c817c135cf09
Fermec28 <fermed28@gmail.com>
2017-11-22 20:44:44 -0500

    Solucion

--- challenges/5-javascript-racer/app.js
+++ challenges/5-javascript-racer/app.js
@@ -1,5 +1,21 @@
-$( "body" ).keyup(function(e) {
 
+var myVar = setInterval(myTimer, 1000);
+var i=6
+var start = false
+function myTimer() {
+	 
+    
+    document.getElementById("demo").innerHTML = i;
+    if (i===0){
+    	clearTimeout(myVar)
+    	start = true	
+    } 
+    i = i-1
+}
+
+
+$( "body" ).keyup(function(e) {
+if (start) {
 	if (e.key==="q"){
 
 		player=$("#player1_strip .active")
@@ -7,7 +23,9 @@ $( "body" ).keyup(function(e) {
 
 		player.next().addClass("active")		
 		if(player.next().hasClass("last")){
+			$("body").css("background-color","blue")
 			alert("Win player1_strip")
+
 		}
 
 
@@ -17,9 +35,10 @@ $( "body" ).keyup(function(e) {
 		player.removeClass("active")
 		player.next().addClass("active")
 		if(player.next().hasClass("last")){
+			$("body").css("background-color","red")
 			alert("Win player2_strip")
 		}
 	}
-	
+	}
   
 });
