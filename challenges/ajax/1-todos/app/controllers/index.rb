33b393306f22908fd4aae62f8804af27ef5ebcfc
Fermedina <fermed28@gmai.com>
2017-11-28 20:06:34 -0500

    Todo create ajaxificado

--- challenges/ajax/1-todos/app/controllers/index.rb
+++ challenges/ajax/1-todos/app/controllers/index.rb
@@ -1,7 +1,17 @@
 get '/' do
+  @posts = Todo.all
   erb :index
 end
 
 post '/add_todo' do
-  p "Dentro de la ruta  /add_todo!"
+  
+  post=  Todo.new(params)
+  if post.save
+  	#El comentario representa como enviar los datos desde back to front con JSON
+  	#content_type :json
+  	#params.to_json
+  	erb :"_post", layout: false, locals: {post: post}
+  end
+  #p params 
+
 end
--- challenges/ajax/1-todos/public/js/application.js
+++ challenges/ajax/1-todos/public/js/application.js
@@ -1,15 +1,27 @@
+
 $(document).ready(function() {
+
   bindEvents();
+  
 });
 
 
 function bindEvents() {
   // Enlaza los eventos que agregan, remuevan y completan TODOS a los elmentos del DOM indicados
-  $('.delete-buton').on('click', delteTot)
-  $(form).on('submit', crateTodo)
+ 
+  $("form").on("submit",crateTodo)
+
+
+  //$('.delete-buton').on('click', delteTot)
+  
+  //$("form").on('submit', crateTodo)
+  console.log("binding .... aplication.js")
 }
 
-function buildTodo(todoName) {
+
+
+
+/*function buildTodo(todoName) {
   // Eso nos ta un pedazo del DOM
   var todoTemplate = $.trim($('#todo_template').html());
   // Creamos un elemento de jquery a partir del template
@@ -18,6 +30,29 @@ function buildTodo(todoName) {
   $todo.find('h2').text(todoName);
   // Devuelve el elemento de jquery para ser usado en otra parte.
   return $todo;
-}
+}*/
 
 // Crea las funciones que añaden, remueven y completan TODOS.
+
+ crateTodo = function(event){
+  
+   // tener el control
+    event.preventDefault()
+    //Tomar la informacion 
+    url= this.action
+    data= {
+      todo_content: $("form .todo").val()} 
+
+    //crar request
+
+    $.ajax({
+      url: url,
+      method: "POST",
+      data: data
+    }).done(function(response){
+        //console.log(response)
+        
+        $(".todoContainer ul").append(response)      
+    })
+  } 
+ 
